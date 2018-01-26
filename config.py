84259f61e3135f016f7400e027c77b6f4850e715
aadrujinin <aadrujinin@yandex.ru>
2018-01-26 16:23:41 +0300

    add value

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 # Этот токен невалидный, можете даже не пробовать :)
-token = '483074230:AAH4Jl7Cef7F0DHbR1hGwKi-BHT6VN43dRg'
+token = '483074230:AAH4Jl7Cef7F0DHbR1hGwKi-BHT6VN43dRg###'
